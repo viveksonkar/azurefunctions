63f2f6f97830ecff759542c39fe866e1e6b92166
Vivek Sonkar <viveksonkar19n@gmail.com>
2020-08-31 04:32:09 +0200

    Sql implementation

--- src/main/java/net/apmoller/crb/azure/function/eventhub/EhProcessor.java
+++ src/main/java/net/apmoller/crb/azure/function/eventhub/EhProcessor.java
@@ -2,6 +2,8 @@ package net.apmoller.crb.azure.function.eventhub;
 
 import com.microsoft.azure.functions.annotation.*;
 import com.microsoft.azure.functions.*;
+
+import java.sql.*;
 import java.util.*;
 
 /**
@@ -13,13 +15,36 @@ public class EhProcessor {
      */
     @FunctionName("EhProcessor")
     public void run(
-        @EventHubTrigger(name = "message", eventHubName = "xml-producer-consume",
+        @EventHubTrigger(name = "message", eventHubName = "xml-processor-eh",
                 connection = "receiverConnectionString",
-                consumerGroup = "$Default", cardinality = Cardinality.MANY) List<String> message,
+                consumerGroup = "$Default", cardinality = Cardinality.MANY, dataType = "string") List<String> message,
+        @TableOutput(name="users", tableName="users", connection="AzureWebJobsStorage") OutputBinding<String> users,
         final ExecutionContext context
     ) {
         context.getLogger().info("Java Event Hub trigger function executed.");
         context.getLogger().info("Length:" + message.size());
-        message.forEach(singleMessage -> context.getLogger().info(singleMessage));
+        message.forEach(singleMessage -> {
+            /*String connectionUrl =
+                    "jdbc:sqlserver://servdemodb.database.windows.net:1433;database=DemoDB;user=Rimi@servdemodb;password={your_password_here};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+
+            ResultSet resultSet = null;
+
+            try (Connection connection = DriverManager.getConnection(connectionUrl);
+                 Statement statement = connection.createStatement();) {
+
+                context.getLogger().info("Message Received :=> " +singleMessage); // This is printed out.
+
+                // Create and execute a SELECT SQL statement.
+                String selectSql = "INSERT USER([name]) values ("+singleMessage+")";
+                context.getLogger().info("Executing SQL :=> " + selectSql);
+                resultSet = statement.executeQuery(selectSql);
+
+            }
+            // Handle any errors that may have occurred.
+            catch ( SQLException e) {
+                e.printStackTrace();
+            }*/
+            context.getLogger().info(singleMessage);
+        } );
     }
 }
